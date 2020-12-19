60ec7991e69f47b92f3da5a6786f734a96afff4d
Sushant <sushant.adhikari.619@gmail.com>
2020-12-19 18:52:38 +0545

    added header section

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,24 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 // Scroll Section active link
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' +sectionId + ']').classList.add('active')
+        } else {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+  
+
